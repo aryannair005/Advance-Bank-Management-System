d26c04d316df529737d11ccae7934d873b96aec3
Aryan Nair <aryanyouofficial3557u@gmail.com>
2026-04-05 12:27:38 +0530

    Get-Balance

--- Backend-Ledger/src/controllers/account.controller.js
+++ Backend-Ledger/src/controllers/account.controller.js
@@ -1,7 +1,6 @@
 const accountModel = require("../models/account.model.js")
 
-
-const createAccount = async(req,res,next) => {
+const createAccount = async(req,res) => {
     const user = req.user
 
     const account = await accountModel.create({
@@ -14,7 +13,37 @@ const createAccount = async(req,res,next) => {
     })
 }
 
+const getAllAccounts = async(req,res) => {
+    const user = req.user
+
+    const accounts = await accountModel.find({user:user._id})
+
+    return res.status(200).json({
+        message:"Accounts fetched successfully",
+        accounts,
+    })
+}
+
+const getAccountBalance = async(req,res) => {
+    const user = req.user
+    const {accountId} = req.params
+
+    const account = await accountModel.findOne({_id:accountId,user:user._id})
+    if(!account){
+        return res.status(404).json({
+            message:"Account not found",
+        })
+    }
+
+    const balance = await account.getBalance()
+    return res.status(200).json({
+        message:"Account balance fetched successfully",
+        balance,
+    }) 
+}
 
 module.exports = {
-    createAccount
+    createAccount,
+    getAllAccounts,
+    getAccountBalance,
 }
--- Backend-Ledger/src/routes/account.routes.js
+++ Backend-Ledger/src/routes/account.routes.js
@@ -3,7 +3,13 @@ const accountRouter = express.Router()
 const authMiddleware = require("../middlewares/auth.middleware.js")
 const accountController = require("../controllers/account.controller.js")
 
-
+// Create Account
 accountRouter.post("/",authMiddleware.authMiddleware,accountController.createAccount)
+
+// Get All Accounts of logged in user
+accountRouter.get("/",authMiddleware.authMiddleware,accountController.getAllAccounts) 
+
+// Get balance of an account
+accountRouter.get("/balance/:accountId",authMiddleware.authMiddleware,accountController.getAccountBalance)  
 
 module.exports = accountRouter
