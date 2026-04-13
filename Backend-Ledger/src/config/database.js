1ba813ca8b5027157f2d930961aca2d131c806d7
Aryan Nair <aryanyouofficial3557u@gmail.com>
2026-04-13 10:06:49 +0530

    db-comment

--- Backend-Ledger/src/config/database.js
+++ Backend-Ledger/src/config/database.js
@@ -1,8 +1,16 @@
-const mongoose = require("mongoose")
+/**
+ * @fileoverview Database connection utility.
+ * This module connects the application to MongoDB using Mongoose.
+ */
 
-const connectToDB = async () =>{
-    await mongoose.connect(process.env.MONGO_URI)
-}
+const mongoose = require("mongoose");
 
+/**
+ * @function connectToDB
+ * @description Establish a connection to MongoDB using the URI from environment variables
+ */
+const connectToDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI);
+};
 
-module.exports = connectToDB
+module.exports = connectToDB;
